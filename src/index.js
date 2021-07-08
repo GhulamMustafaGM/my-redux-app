977c8ca2d9bf81e2fbd06f191c946fd633c26de4
Ghulam Mustafa <mustafaji@gmail.com>
2021-07-08 19:19:42 +0200

    updated index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 //default state
 var defaultState = 0;
@@ -21,21 +22,19 @@ const balanceReducer = (state = defaultState, action) => {
 };
 
 //store
-var store = createStore(balanceReducer);
-console.log(store.getState());
+var store = createStore(balanceReducer, composeWithDevTools());
+store.subscribe( () => {
+  console.log(store.getState()); //get updated state
+});
 
 //dispatch
 store.dispatch({ type: "abc"}); //dispatch: pass an action to store; it invokes the reducer automatically
-console.log(store.getState()); //get updated state
 
 store.dispatch({ type: "DEPOSIT"});
-console.log(store.getState()); //100
 
 store.dispatch({ type: "DEPOSIT"});
-console.log(store.getState()); //200
 
 store.dispatch({ type: "WITHDRAW"});
-console.log(store.getState()); //100
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
